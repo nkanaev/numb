4d34cea7ae984fb7dcef59463432d256cd8dcff7
nkanaev <nkanaev@live.com>
2021-04-14 10:35:41 +0100

    repl skeleton

--- numb.go
+++ numb.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var prompt = "> "
+
+type Value struct {
+	num float64
+}
+
+func (v *Value) String() string {
+	return strconv.FormatInt(int64(v.num), 10)
+}
+
+func eval(expr string, vars map[string]Value) *Value {
+	return nil	
+}
+
+func repl() {
+	vars := make(map[string]Value)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("enter `q` to quit")
+	fmt.Print(prompt)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "q" {
+			break
+		}
+		val := eval(line, vars)
+		if val != nil {
+			fmt.Println(" ", val.String())
+		}
+		fmt.Print(prompt)
+	}
+}
 
 func main() {
-	fmt.Println("soon")
+	repl()
 }
