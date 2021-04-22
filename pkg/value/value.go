e6482852612101f7a080fdac5fe0abcbb8187852
nkanaev <nkanaev@live.com>
2021-04-22 21:00:00 +0100

    numeric system state

--- pkg/value/value.go
+++ pkg/value/value.go
@@ -1,9 +1,24 @@
 package value
 
-import "math/big"
+import (
+	"fmt"
+	"math/big"
+	"strings"
+)
+
+type NumeralSystem int
+
+const (
+	DEC NumeralSystem = iota
+	HEX
+	OCT
+	BIN
+	RAT
+)
 
 type Value struct {
-	Num *big.Rat
+	Num  *big.Rat
+	Fmt NumeralSystem
 }
 
 func NewInt(x int64) Value {
@@ -15,75 +30,93 @@ func Parse(x string) Value {
 	if !ok {
 		panic("unable to parse number: " + x)
 	}
-	return Value{Num: num}
+	base := DEC
+	if strings.HasPrefix(x, "0x") {
+		base = HEX
+	} else if strings.HasPrefix(x, "0o") {
+		base = OCT
+	} else if strings.HasPrefix(x, "0b") {
+		base = BIN
+	}
+	return Value{Num: num, Fmt: base}
 }
 
 func (a Value) Mul(b Value) Value {
-	return Value{Num: new(big.Rat).Mul(a.Num, b.Num)}
+	return Value{Num: new(big.Rat).Mul(a.Num, b.Num), Fmt: a.Fmt}
 }
 
 func (a Value) Add(b Value) Value {
-	return Value{Num: new(big.Rat).Add(a.Num, b.Num)}
+	return Value{Num: new(big.Rat).Add(a.Num, b.Num), Fmt: a.Fmt}
 }
 
 func (a Value) Sub(b Value) Value {
-	return Value{Num: new(big.Rat).Sub(a.Num, b.Num)}
+	return Value{Num: new(big.Rat).Sub(a.Num, b.Num), Fmt: a.Fmt}
 }
 
 func (a Value) Quo(b Value) Value {
-	return Value{Num: new(big.Rat).Quo(a.Num, b.Num)}
+	return Value{Num: new(big.Rat).Quo(a.Num, b.Num), Fmt: a.Fmt}
 }
 
 func (a Value) Lsh(b Value) Value {
 	ia, ib := a.Num.Num(), uint(b.Num.Num().Uint64())
 	num := big.NewRat(1, 1)
 	num.Num().Lsh(ia, ib)
-	return Value{Num: num}
+	return Value{Num: num, Fmt: a.Fmt}
 }
 
 func (a Value) Rsh(b Value) Value {
 	ia, ib := a.Num.Num(), uint(b.Num.Num().Uint64())
 	num := big.NewRat(1, 1)
 	num.Num().Rsh(ia, ib)
-	return Value{Num: num}
+	return Value{Num: num, Fmt: a.Fmt}
 }
 
 func (a Value) And(b Value) Value {
 	num := big.NewRat(1, 1)
 	num.Num().And(a.Num.Num(), b.Num.Num())
-	return Value{Num: num}
+	return Value{Num: num, Fmt: a.Fmt}
 }
 
 func (a Value) Or(b Value) Value {
 	num := big.NewRat(1, 1)
 	num.Num().Or(a.Num.Num(), b.Num.Num())
-	return Value{Num: num}
+	return Value{Num: num, Fmt: a.Fmt}
 }
 
 func (a Value) Xor(b Value) Value {
 	num := big.NewRat(1, 1)
 	num.Num().Xor(a.Num.Num(), b.Num.Num())
-	return Value{Num: num}
+	return Value{Num: num, Fmt: a.Fmt}
 }
 
 func (a Value) Rem(b Value) Value {
 	num := big.NewRat(1, 1)
 	num.Num().Rem(a.Num.Num(), b.Num.Num())
-	return Value{Num: num}
+	return Value{Num: num, Fmt: a.Fmt}
 }
 
 func (a Value) Exp(b Value) Value {
 	num := big.NewRat(1, 1)
 	num.Num().Exp(a.Num.Num(), b.Num.Num(), nil)
-	return Value{Num: num}
+	return Value{Num: num, Fmt: a.Fmt}
 }
 
 func (a Value) Neg() Value {
-	return Value{Num: new(big.Rat).Neg(a.Num)}
+	return Value{Num: new(big.Rat).Neg(a.Num), Fmt: a.Fmt}
 }
 
 func (a Value) String() string {
-	return a.Num.RatString()
+	switch a.Fmt {
+	case DEC:
+		return a.Num.RatString()
+	case HEX:
+		return fmt.Sprintf("%#x", a.Num.Num())
+	case OCT:
+		return fmt.Sprintf("%O", a.Num.Num())
+	case BIN:
+		return fmt.Sprintf("%#b", a.Num.Num())
+	}
+	return a.Num.String()
 }
 
 func (a Value) Eval(map[string]Value) Value {
