36b1fbfe20db6b8fca3002855e63a6c6d63146ca
nkanaev <nkanaev@live.com>
2021-05-05 22:28:39 +0100

    more trigonometry functions

--- pkg/funcs/funcs.go
+++ pkg/funcs/funcs.go
@@ -6,6 +6,11 @@ type Func func(...value.Value) value.Value
 
 var db = map[string]Func{
 	"sin": Sin,
+	"cos": Cos,
+	"tan": Tan,
+	"asin": Asin,
+	"acos": Acos,
+	"atan": Atan,
 }
 
 func Get(x string) *Func {
--- pkg/funcs/trigonometry.go
+++ pkg/funcs/trigonometry.go
@@ -8,19 +8,51 @@ import (
 	"github.com/nkanaev/numb/pkg/value"
 )
 
-func Sin(args ...value.Value) value.Value {
+type mathOp func(float64) float64
+
+func trigOp1(name string, op mathOp, args ...value.Value) value.Value {
+	if len(args) != 1 {
+		panic(name + ": expected 1 argument")
+	}
 	arg := args[0]
 	if arg.Unit == nil {
-		panic("provide rad or deg unit")
+		panic(name + ": provide rad or deg unit")
 	}
 	u := arg.Unit.String()
 	if u != "deg" && u != "rad" {
-		panic("expected rad or deg unit")
+		panic(name + ": expected rad or deg unit")
 	}
 	if u == "deg" {
 		arg = arg.To(unit.Get("rad"))
 	}
-	f64, _ := arg.Num.Float64()
-	num := new(big.Rat).SetFloat64(math.Sin(f64))
-	return value.Value{Num: num}
+	f, exact := arg.Num.Float64()
+	if !exact && math.IsInf(f, 0) {
+		panic(name + ": value too large")
+	}
+	num := new(big.Rat).SetFloat64(op(f))
+	return value.Value{Num: num, Unit: arg.Unit}
+}
+
+func Sin(args ...value.Value) value.Value {
+	return trigOp1("sin", math.Sin, args...)
+}
+
+func Cos(args ...value.Value) value.Value {
+	return trigOp1("cos", math.Cos, args...)
+}
+
+func Tan(args ...value.Value) value.Value {
+	return trigOp1("tan", math.Tan, args...)
+}
+
+func Asin(args ...value.Value) value.Value {
+	return trigOp1("asin", math.Asin, args...)
+}
+
+func Acos(args ...value.Value) value.Value {
+	return trigOp1("acos", math.Acos, args...)
+}
+
+func Atan(args ...value.Value) value.Value {
+	return trigOp1("atan", math.Atan, args...)
 }
