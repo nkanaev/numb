b8fa0c23d3aea5f8786688e6d94c1cf9d63c5ee9
Nazar Kanaev <nkanaev@live.com>
2021-05-09 13:48:38 +0100

    more volume units

--- pkg/unit/units_volume.go
+++ pkg/unit/units_volume.go
@@ -10,10 +10,39 @@ var volumeUnits = []baseUnit{
 		prefixpow:    3,
 	},
 	{
-		name:      "l",
-		aliases:   []string{"liter", "litre"},
-		value:     f64(0.001),
+		name:         "l",
+		shortaliases: []string{"lt"},
+		aliases:      []string{"liter", "litre"},
+		value:        f64(0.001),
+		dimension:    VOLUME,
+		prefixes:     &metricPrefixes,
+	},
+	{
+		name:         "in³",
+		shortaliases: []string{"in3", "cuin"},
+		value:        f64(1.6387064e-5),
+		dimension:    VOLUME,
+	},
+	{
+		name:         "ft³",
+		shortaliases: []string{"ft3", "cuft"},
+		value:        f64(0.028316846592),
+		dimension:    VOLUME,
+	},
+	{
+		name:         "yd³",
+		shortaliases: []string{"yd3", "cuyd"},
+		value:        f64(0.764554857984),
+		dimension:    VOLUME,
+	},
+	{
+		name:      "teaspoon",
+		value:     f64(0.000005),
 		dimension: VOLUME,
-		prefixes:  &metricPrefixes,
+	},
+	{
+		name:      "tablespoon",
+		value:     f64(0.000015),
+		dimension: VOLUME,
 	},
 }
