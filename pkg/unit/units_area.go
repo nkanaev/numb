1ecd1ec4d3b8e7f0d30f49188c7965124677967f
Nazar Kanaev <nkanaev@live.com>
2021-05-09 13:37:25 +0100

    more area units

--- pkg/unit/units_area.go
+++ pkg/unit/units_area.go
@@ -9,4 +9,48 @@ var areaUnits = []baseUnit{
 		prefixes:     &metricPrefixes,
 		prefixpow:    2,
 	},
+	{
+		name:      "sqin",
+		value:     f64(0.00064516),
+		dimension: AREA,
+	},
+	{
+		name:      "sqft",
+		aliases:   []string{"sqfeet"},
+		value:     f64(0.09290304),
+		dimension: AREA,
+	},
+	{
+		name:      "sqyd",
+		aliases:   []string{"sqyard"},
+		value:     f64(0.83612736),
+		dimension: AREA,
+	},
+	{
+		name:      "sqrd",
+		value:     f64(25.29295),
+		dimension: AREA,
+	},
+	{
+		name:      "sqch",
+		value:     f64(404.6873),
+		dimension: AREA,
+	},
+	{
+		name:         "sqmi",
+		shortaliases: []string{"sqmil"},
+		aliases:      []string{"sqmile"},
+		value:        f64(6.4516e-10),
+		dimension:    AREA,
+	},
+	{
+		name:      "acre",
+		value:     f64(4046.86),
+		dimension: AREA,
+	},
+	{
+		name:      "hectare",
+		value:     f64(10000),
+		dimension: AREA,
+	},
 }
