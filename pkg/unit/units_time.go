1896f5c02e92319f0ecd81c2a9e813003d0d69ad
nkanaev <nkanaev@live.com>
2021-05-10 11:48:13 +0100

    more time units

--- pkg/unit/units_time.go
+++ pkg/unit/units_time.go
@@ -22,26 +22,42 @@ var timeUnits = []baseUnit{
 	},
 	{
 		name:      "day",
-		aliases:   []string{},
 		value:     f64(86400),
 		dimension: TIME,
 	},
 	{
 		name:      "week",
-		aliases:   []string{},
 		value:     f64(7 * 86400),
 		dimension: TIME,
 	},
 	{
 		name:      "month",
-		aliases:   []string{},
 		value:     f64(2629800),
 		dimension: TIME,
+		description: "1/12th of Julian Year",
 	},
 	{
 		name:      "year",
-		aliases:   []string{},
 		value:     f64(31557600),
 		dimension: TIME,
+		description: "Julian Year (365.25 days)",
+	},
+	{
+		name:      "decade",
+		value:     f64(315576000),
+		dimension: TIME,
+		description: "Julian decade",
+	},
+	{
+		name:      "century",
+		value:     f64(3155760000),
+		dimension: TIME,
+		description: "Julian century",
+	},
+	{
+		name:      "millenium",
+		value:     f64(31557600000),
+		dimension: TIME,
+		description: "Julian millenium",
 	},
 }
