c99f60548302ab44f17c24dc158d1e28fe284eef
nkanaev <nkanaev@live.com>
2021-05-12 09:30:34 +0100

    update temperature names

--- pkg/unit/units_temperature.go
+++ pkg/unit/units_temperature.go
@@ -4,24 +4,26 @@ import "math/big"
 
 var temperatureUnits = []baseUnit{
 	{
-		name:      "k",
+		name:      "K",
 		aliases:   []string{"kelvin"},
 		value:     f64(1),
 		dimension: TEMPERATURE,
 		prefixes:  &metricPrefixes,
 	},
 	{
-		name:      "c",
-		aliases:   []string{"celsius"},
-		value:     f64(1),
-		offset:    f64(273.15),
-		dimension: TEMPERATURE,
+		name:         "°C",
+		shortaliases: []string{"degC"},
+		aliases:      []string{"celsius"},
+		value:        f64(1),
+		offset:       f64(273.15),
+		dimension:    TEMPERATURE,
 	},
 	{
-		name:      "f",
-		aliases:   []string{"fahrenheit"},
-		value:     big.NewRat(10, 18),
-		offset:    f64(459.67),
-		dimension: TEMPERATURE,
+		name:         "°F",
+		shortaliases: []string{"degF"},
+		aliases:      []string{"fahrenheit"},
+		value:        big.NewRat(10, 18),
+		offset:       f64(459.67),
+		dimension:    TEMPERATURE,
 	},
 }
