dcb0e0875a1a6377a1a60f137403162fb19e01ef
nkanaev <nkanaev@live.com>
2021-08-10 13:48:40 +0100

    parse e notation

--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -33,12 +33,14 @@ func (s *Scanner) char() rune {
 	return s.src[s.cur]
 }
 
-func (s *Scanner) nextChar() {
+func (s *Scanner) next() {
 	s.cur += 1
 }
 
-func (s *Scanner) next() {
-	for ; unicode.IsSpace(s.char()); s.nextChar() {
+func isDecimal(ch rune) bool { return '0' <= ch && ch <= '9' }
+
+func (s *Scanner) scan() {
+	for ; unicode.IsSpace(s.char()); s.next() {
 	}
 
 	if s.cur >= len(s.src) {
@@ -48,90 +50,37 @@ func (s *Scanner) next() {
 
 	ch := s.char()
 	switch {
-	case unicode.IsDigit(ch):
-		separators := ",_"
-		accept := "0123456789"
-		prefix := ""
-		digits := make([]rune, 0)
-		if ch == '0' {
-			s.nextChar()
-			ch = s.char()
-			if ch == 'b' {
-				prefix = "0b"
-				accept = "01"
-				s.nextChar()
-			} else if ch == 'x' {
-				prefix = "0x"
-				accept = "0123456789abcdefABCDEF"
-				s.nextChar()
-			} else if ch == 'o' {
-				prefix = "0o"
-				accept = "01234567"
-				s.nextChar()
-			} else if ch == '.' {
-				digits = append(digits, '0')
-				digits = append(digits, '.')
-				s.nextChar()
-				for ; strings.ContainsRune(accept, s.char()); s.nextChar() {
-					digits = append(digits, s.char())
-				}
-				s.Token = token.NUM
-				s.Value = prefix + string(digits)
-				return
-			} else if !unicode.IsDigit(ch) {
-				s.Token = token.NUM
-				s.Value = "0"
-				return
-			}
-		}
-		acceptChars := accept + separators
-		for ; strings.ContainsRune(acceptChars, s.char()); s.nextChar() {
-			if strings.ContainsRune(separators, s.char()) {
-				continue
-			}
-			digits = append(digits, s.char())
-		}
-		if prefix == "" && s.char() == '.' {
-			digits = append(digits, s.char())
-			s.nextChar()
-			for ; strings.ContainsRune(acceptChars, s.char()); s.nextChar() {
-				if strings.ContainsRune(separators, s.char()) {
-					continue
-				}
-				digits = append(digits, s.char())
-			}
-		}
-		s.Token = token.NUM
-		s.Value = prefix + string(digits)
+	case isDecimal(ch):
+		s.Token, s.Value = s.scanNumber()
 	case ch == '=':
 		s.Token = token.ASSIGN
-		s.nextChar()
+		s.next()
 	case ch == ':':
 		s.Token = token.COLON
-		s.nextChar()
+		s.next()
 	case ch == '(':
 		s.Token = token.LPAREN
-		s.nextChar()
+		s.next()
 	case ch == ')':
 		s.Token = token.RPAREN
-		s.nextChar()
+		s.next()
 	case ch == ',':
 		s.Token = token.COMMA
-		s.nextChar()
+		s.next()
 	case ch == '^':
 		s.Token = token.EXP
-		s.nextChar()
+		s.next()
 	case ch == '*' || ch == '/' || ch == '+' || ch == '-':
 		s.Token = token.StringToOperator[string(ch)]
-		s.nextChar()
+		s.next()
 	case ch == '<' || ch == '>':
-		s.nextChar()
+		s.next()
 		if s.char() != ch {
 			s.cur -= 1
 			s.Token = token.Illegal
 			return
 		}
-		s.nextChar()
+		s.next()
 		switch ch {
 		case '<':
 			s.Token = token.SHL
@@ -146,10 +95,10 @@ func (s *Scanner) next() {
 			}
 			letters = append(letters, s.char())
 			if s.char() == '}' {
-				s.nextChar()
+				s.next()
 				break
 			}
-			s.nextChar()
+			s.next()
 		}
 		s.Token = token.WORD
 		s.Value = string(letters)
@@ -157,7 +106,7 @@ func (s *Scanner) next() {
 		letters := make([]rune, 0)
 		for ch != 0 && !unicode.IsSpace(ch) && !strings.Contains("^*/+-()=:", string(ch)) {
 			letters = append(letters, ch)
-			s.nextChar()
+			s.next()
 			ch = s.char()
 		}
 		word := string(letters)
@@ -177,8 +126,85 @@ func (s *Scanner) next() {
 	}
 }
 
+func (s *Scanner) digits(base int) string {
+	separators := ",_"
+	digits := make([]rune, 0)
+	accept := "0123456789abcdef"[:base]
+	if base == 16 {
+		accept += "ABCDEF"
+	}
+	loop: for  {
+		ch := s.char()
+		switch {
+		case strings.ContainsRune(accept, ch):
+			digits = append(digits, s.char())
+			s.next()
+		case strings.ContainsRune(separators, ch):
+			s.next()
+		default:
+			break loop
+		}
+	}
+	return string(digits)
+}
+
+func (s *Scanner) scanNumber() (token.Token, string) {
+	value := ""
+	base := 10
+
+	// integer
+	if s.char() == '0' {
+		s.next()
+		switch s.char() {
+		case 'x', 'X':
+			s.next()
+			base = 16
+			value = "0x"
+		case 'o', 'O':
+			s.next()
+			base = 8
+			value = "0o"
+		case 'b', 'B':
+			s.next()
+			base = 2
+			value = "0b"
+		default:
+			value = "0"
+		}
+	}
+	value += s.digits(base)
+
+	// HACK: remove leading 0 from decimal numbers to avoid confusion with octal
+	// 0123 = 123 (!= 0o123)
+	if len(value) > 1 && value[0] == '0' && base == 10 {
+		value = value[1:]
+	}
+
+	// fraction
+	if base == 10 && s.char() == '.' {
+		s.next()
+		value += "." + s.digits(base)
+	}
+
+	// exponent
+	if base == 10 && s.char() == 'e' {
+		s.next()
+		switch s.char() {
+		case '-':
+			s.next()
+			value += "e-" + s.digits(base)
+		case '+':
+			s.next()
+			fallthrough
+		default:
+			value += "e" + s.digits(base)
+		}
+	}
+	return token.NUM, value
+}
+
 func (s *Scanner) Scan() bool {
 	s.Value = ""
-	s.next()
+	s.scan()
 	return s.Token != token.Illegal && s.Token != token.END
 }
