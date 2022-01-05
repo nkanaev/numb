792d0c4965d8dc5cadb4761d0c42a9860698b7ee
Nazar Kanaev <nkanaev@live.com>
2022-01-05 11:00:15 +0000

    time operations

--- pkg/value/time.go
+++ pkg/value/time.go
@@ -2,9 +2,12 @@ package value
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
+	"github.com/nkanaev/numb/pkg/dimension"
 	"github.com/nkanaev/numb/pkg/token"
+	"github.com/nkanaev/numb/pkg/unit"
 )
 
 type Time struct {
@@ -13,6 +16,25 @@ type Time struct {
 }
 
 func (a Time) BinOP(op token.Token, b Value) (Value, error) {
+	if Type(b) == TYPE_UNIT {
+		b := b.(Unit)
+		measure, _ := b.Units.Dimension().Measure()
+		if measure != dimension.TIME {
+			return nil, fmt.Errorf("%s is not a measure of %s", b, dimension.TIME)
+		}
+
+		diff_nsec := unit.Convert(b.Num, b.Units, unit.Must("nanosecond"))
+		if !diff_nsec.IsInt() {
+			return nil, fmt.Errorf("cannot deal with fractional nanosecs")
+		}
+		diff_int64 := time.Duration(diff_nsec.Num().Int64())
+
+		if op == token.ADD {
+			return Time{ts: a.ts.Add(diff_int64), fmt: a.fmt}, nil
+		} else if op == token.SUB {
+			return Time{ts: a.ts.Add(-diff_int64), fmt: a.fmt}, nil
+		}
+	}
 	return nil, UnsupportedBinOP{a: a, b: b, op: op}
 }
 
