7f3ad402be859fc17e4ef586c88a509ad89a83da
Nazar Kanaev <nkanaev@live.com>
2022-01-08 13:58:12 +0000

    update todo

--- pkg/value/value.go
+++ pkg/value/value.go
@@ -68,6 +68,7 @@ func GetNamedTime(name string) *Time {
 		return &Time{ts: time.Now(), fmt: time.ANSIC}
 	case "time":
 		return &Time{ts: time.Now(), fmt: "15:04"}
+	// TODO: date
 	}
 	return nil
 }
