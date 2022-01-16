c1ccc77c86d636f70d8233724edc9498ac08008d
Nazar Kanaev <nkanaev@live.com>
2022-01-16 16:36:28 +0000

    timezone matching

--- pkg/value/time.go
+++ pkg/value/time.go
@@ -3,7 +3,11 @@ package value
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 	"time"
+	"unicode"
 
 	"github.com/nkanaev/numb/pkg/dimension"
 	"github.com/nkanaev/numb/pkg/token"
@@ -45,13 +49,85 @@ func (t Time) UnOP(op token.Token) (Value, error) {
 }
 
 func (t Time) In(fmt string) (Value, error) {
-	// TODO: case-insensitive tz match
-	if loc, _ := time.LoadLocation(fmt); loc != nil {
+	loc, err := getLocation(fmt)
+	if loc != nil {
 		return Time{ts: t.ts.In(loc), fmt: t.fmt}, nil
 	}
-	return nil, errors.New("unrecognized time format: " + fmt)
+	if err != nil {
+		return nil, err
+	}
+	return nil, errors.New("unknown timezone: " + fmt)
 }
 
 func (t Time) String() string {
 	return t.ts.Format(t.fmt)
+}
+
+func getLocation(name string) (*time.Location, error) {
+	if strings.EqualFold(name, "utc") {
+		return time.UTC, nil
+	}
+	if strings.EqualFold(name, "local") {
+		return time.Local, nil
+	}
+
+	var zoneDirs = []string{
+		"/usr/share/zoneinfo",
+		"/usr/share/lib/zoneinfo",
+		"/usr/lib/locale/TZ",
+	}
+	for _, dir := range zoneDirs {
+		if loc := findLocationFromDir(name, dir, ""); loc != nil {
+			return loc, nil
+		}
+	}
+	return nil, nil
+}
+
+func findLocationFromDir(name, rootdir, subdir string) *time.Location {
+    files, _ := ioutil.ReadDir(rootdir + "/" + subdir)
+
+    for _, f := range files {
+        if !unicode.IsUpper(rune(f.Name()[0])) {
+            continue
+        }
+
+        if f.IsDir() && len(subdir) == 0 {
+			if loc := findLocationFromDir(name, rootdir, subdir + "/" + f.Name()); loc != nil {
+				return loc
+			}
+        } else {
+			tzname := strings.TrimLeft(subdir + "/" + f.Name(), "/")
+			if tzNameMatches(name, tzname) {
+				if tzdata, err := os.ReadFile(rootdir + "/" + tzname); err == nil {
+					if tzinfo, err := time.LoadLocationFromTZData(tzname, tzdata); err == nil {
+						return tzinfo
+					}
+				}
+			}
+        }
+    }
+	return nil
+}
+
+func normalizeTZName(name string) string {
+	name = strings.ReplaceAll(name, "_", "")
+	name = strings.ReplaceAll(name, "-", "")
+	name = strings.ToLower(name)
+	return name
+}
+
+func tzNameMatches(name, tzname string) bool {
+	name = normalizeTZName(name)
+	tzname = normalizeTZName(tzname)
+
+	if strings.ContainsRune(tzname, '/') && !strings.ContainsRune(name, '/') {
+		chunks := strings.Split(tzname, "/")
+		tzname := chunks[len(chunks)-1]
+		if strings.EqualFold(name, tzname) {
+			return true
+		}
+	}
+
+	return strings.EqualFold(name, tzname)
 }
