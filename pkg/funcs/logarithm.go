941151f09991af155fdddf31384d8c927a2b1f97
Nazar Kanaev <nkanaev@live.com>
2022-01-18 17:16:06 +0000

    refactor funcs

--- pkg/funcs/logarithm.go
+++ pkg/funcs/logarithm.go
@@ -7,37 +7,37 @@ import (
 	"github.com/nkanaev/numb/pkg/value"
 )
 
-func arity(want, have int) {
-	if want != have {
-		panic(fmt.Sprintf("expected %d arguments, got %d", want, have))
+func logF(fn func(float64) float64, args ...value.Value) (value.Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("expected 1 argument, got %d", len(args))
 	}
-}
+	arg := args[0]
 
-func f64(val value.Value) float64 {
-	// TODO: type casting fixes
-	f, exact := val.(value.Number).Num.Float64()
-	if !exact && math.IsInf(f, 0) {
-		panic(fmt.Sprintf("%s magnitude is too large", val.String()))
+	num, ok := arg.(value.Number)
+	if !ok {
+		return nil, fmt.Errorf("expected number, got %s", value.Type(arg))
 	}
-	return f
+
+	f64, exact := num.Num.Float64()
+	if !exact && math.IsInf(f64, 0) {
+		return nil, fmt.Errorf("%s magnitude is too large", arg)
+	}
+
+	return value.Float64(fn(f64)), nil
 }
 
 func Log(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	return value.Float64(math.Log(f64(args[0]))), nil
+	return logF(math.Log, args...)
 }
 
 func Log2(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	return value.Float64(math.Log2(f64(args[0]))), nil
+	return logF(math.Log2, args...)
 }
 
 func Log10(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	return value.Float64(math.Log10(f64(args[0]))), nil
+	return logF(math.Log10, args...)
 }
 
 func Sqrt(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	return value.Float64(math.Sqrt(f64(args[0]))), nil
+	return logF(math.Sqrt, args...)
 }
--- pkg/funcs/trigonometry.go
+++ pkg/funcs/trigonometry.go
@@ -1,7 +1,9 @@
 package funcs
 
 import (
+	"fmt"
 	"math"
+	"math/big"
 
 	"github.com/nkanaev/numb/pkg/unit"
 	"github.com/nkanaev/numb/pkg/value"
@@ -25,35 +27,64 @@ func asRadian(val value.Number) value.Value {
 	return value.Unit{Num: val.Num, Units: unit.Must("rad")}
 }
 
+func trigOp(fn func(float64) float64, inUnit, outUnit bool, args ...value.Value) (value.Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("expected 1 argument, got %d", len(args))
+	}
+	arg := args[0]
+
+	var num *big.Rat
+	switch arg.(type) {
+	case value.Number:
+		num = arg.(value.Number).Num
+	case value.Unit:
+		if !inUnit {
+			return nil, fmt.Errorf("expected number, got %s", value.Type(arg))
+		}
+		arg := arg.(value.Unit)
+		var err error
+		num, err = unit.Convert(arg.Num, arg.Units, unit.Must("rad"))
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("unsupported type: %s", value.Type(arg))
+	}
+
+	f64, exact := num.Float64()
+	if !exact && math.IsInf(f64, 0) {
+		return nil, fmt.Errorf("%s magnitude is too large", arg)
+	}
+
+	if outUnit {
+		return value.Unit{
+			Num: new(big.Rat).SetFloat64(fn(f64)),
+			Units: unit.Must("rad"),
+		}, nil
+	}
+	return value.Float64(fn(f64)), nil
+}
+
 func Sin(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	arg := toRadian(args[0])
-	return value.Float64(math.Sin(f64(arg))), nil
+	return trigOp(math.Sin, true, false, args...)
 }
 
 func Cos(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	arg := toRadian(args[0])
-	return value.Float64(math.Cos(f64(arg))), nil
+	return trigOp(math.Cos, true, false, args...)
 }
 
 func Tan(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	arg := toRadian(args[0])
-	return value.Float64(math.Cos(f64(arg))), nil
+	return trigOp(math.Tan, true, false, args...)
 }
 
 func Asin(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	return asRadian(value.Float64(math.Asin(f64(args[0])))), nil
+	return trigOp(math.Asin, false, true, args...)
 }
 
 func Acos(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	return asRadian(value.Float64(math.Acos(f64(args[0])))), nil
+	return trigOp(math.Acos, false, true, args...)
 }
 
 func Atan(args ...value.Value) (value.Value, error) {
-	arity(1, len(args))
-	return asRadian(value.Float64(math.Atan(f64(args[0])))), nil
+	return trigOp(math.Atan, false, true, args...)
 }
