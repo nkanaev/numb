10b7a04d76b4a3e56af7c700c26531bbf8170a69
Nazar Kanaev <nkanaev@live.com>
2022-01-27 13:49:12 +0000

    more date formats

--- pkg/timeutil/parse.go
+++ pkg/timeutil/parse.go
@@ -10,11 +10,21 @@ var dateLayouts = []string{
 	"2006/01/02",
 	"2006.01.02",
 
+	"02 Jan 2006",
+	"_2 Jan 2006",
+	"02 January 2006",
+	"_2 January 2006",
+
 	"01/02/2006",
 	"01.02.2006",
 
 	"2006-01-02T15:04",
 	"2006/01/02 15:04",
+
+	"02 Jan 2006 15:04",
+	"_2 Jan 2006 15:04",
+	"02 January 2006 15:04",
+	"_2 January 2006 15:04",
 }
 
 var unknownFormat = errors.New("unknown time format")
--- pkg/timeutil/parse_test.go
+++ pkg/timeutil/parse_test.go
@@ -19,10 +19,22 @@ func TestParse(t *testing.T) {
 		// DD MM YYYY
 		{"05/06/2019", time.Date(2019, time.May, 6, 0, 0, 0, 0, tz)},
 		{"05.06.2019", time.Date(2019, time.May, 6, 0, 0, 0, 0, tz)},
+		{"06 Jun 2019", time.Date(2019, time.June, 6, 0, 0, 0, 0, tz)},
+		{"6 Jun 2019", time.Date(2019, time.June, 6, 0, 0, 0, 0, tz)},
+		{"06 June 2019", time.Date(2019, time.June, 6, 0, 0, 0, 0, tz)},
+		{"6 June 2019", time.Date(2019, time.June, 6, 0, 0, 0, 0, tz)},
+		{"06 june 2019", time.Date(2019, time.June, 6, 0, 0, 0, 0, tz)},
+		{"6 june 2019", time.Date(2019, time.June, 6, 0, 0, 0, 0, tz)},
 
 		// YYYY MM DD HH MM
 		{"2019-05-06T13:30", time.Date(2019, time.May, 6, 13, 30, 0, 0, tz)},
 		{"2019/05/06 13:30", time.Date(2019, time.May, 6, 13, 30, 0, 0, tz)},
+		{"06 Jun 2019 13:30", time.Date(2019, time.June, 6, 13, 30, 0, 0, tz)},
+		{"6 Jun 2019 13:30", time.Date(2019, time.June, 6, 13, 30, 0, 0, tz)},
+		{"06 June 2019 13:30", time.Date(2019, time.June, 6, 13, 30, 0, 0, tz)},
+		{"6 June 2019 13:30", time.Date(2019, time.June, 6, 13, 30, 0, 0, tz)},
+		{"06 june 2019 13:30", time.Date(2019, time.June, 6, 13, 30, 0, 0, tz)},
+		{"6 june 2019 13:30", time.Date(2019, time.June, 6, 13, 30, 0, 0, tz)},
 	}
 
 	for _, tcase := range tcases {
